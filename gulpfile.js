fa5202f1488c64cefc26929cbd6419506e4cb457
Hyyper <hypercomps@gmail.com>
2018-12-20 13:09:08 -0800

    Fixed issues with gulp v4.0+

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-const SETTINGS     = require('./gulp-settings.js');
-const GULP         = require('gulp');
-const SASS         = require('gulp-sass');
-const CONCAT       = require('gulp-concat');
-const UGLIFYCSS    = require('gulp-uglifycss');
-const BABEL        = require('gulp-babel');
-const NOTIFY       = require('gulp-notify');
-const PLUMBER      = require('gulp-plumber');
-const BROWSER_SYNC = require('browser-sync').create();
-const SOURCEMAPS   = require('gulp-sourcemaps');
-const POSTCSS      = require('gulp-postcss');
-const AUTOPREFIXER = require('autoprefixer');
-const CLEAN        = require('del');
-const IMAGEMIN     = require('gulp-imagemin');
-const WAIT         = require('gulp-wait');
-const CACHE        = require('gulp-cache');
+const SETTINGS     = require('./gulp-settings.js')
+const GULP         = require('gulp')
+const SASS         = require('gulp-sass')
+const CONCAT       = require('gulp-concat')
+const UGLIFYCSS    = require('gulp-uglifycss')
+const BABEL        = require('gulp-babel')
+const NOTIFY       = require('gulp-notify')
+const PLUMBER      = require('gulp-plumber')
+const BROWSER_SYNC = require('browser-sync').create()
+const SOURCEMAPS   = require('gulp-sourcemaps')
+const POSTCSS      = require('gulp-postcss')
+const AUTOPREFIXER = require('autoprefixer')
+const CLEAN        = require('del')
+const IMAGEMIN     = require('gulp-imagemin')
+const WAIT         = require('gulp-wait')
+const CACHE        = require('gulp-cache')
 
 function handleError(error) {
 	console.log(error.toString())
@@ -22,7 +22,7 @@ function handleError(error) {
 
 const POSTCSS_PLUGINS = [
 	AUTOPREFIXER({ browsers: ['last 4 version', 'ie >= 10', 'Android 4'] })
-];
+]
 
 //local server
 GULP.task('server', function() {
@@ -33,12 +33,12 @@ GULP.task('server', function() {
 			directory: true,
 			reloadOnRestart: true
 		}
-	});
+	})
 })
 
 GULP.task('sass:dev', function() {
-	return GULP.src(SETTINGS.src.scss + "**/*.+(scss|sass)")
-		.pipe(PLUMBER({errorHandler: NOTIFY.onError("Error: <%= error.message %>")}))
+	return GULP.src(SETTINGS.src.scss + '**/*.+(scss|sass)')
+		.pipe(PLUMBER({errorHandler: NOTIFY.onError('Error: <%= error.message %>')}))
 		.pipe(WAIT(500))
 		.pipe(SOURCEMAPS.init())
 		.pipe(SASS({outputStyle: 'compressed'}))
@@ -46,16 +46,16 @@ GULP.task('sass:dev', function() {
 		.pipe(SOURCEMAPS.write())
 		.pipe(CONCAT('style.css'))
 		.pipe(GULP.dest('./' + SETTINGS.dist.css))
-		.pipe(BROWSER_SYNC.reload({stream: true}));
+		.pipe(BROWSER_SYNC.reload({stream: true}))
 })
 
 GULP.task('sass:dist', function() {
-	return GULP.src(SETTINGS.src.scss + "**/*.+(scss|sass)")
-		.pipe(PLUMBER({errorHandler: NOTIFY.onError("Error: <%= error.message %>")}))
+	return GULP.src(SETTINGS.src.scss + '**/*.+(scss|sass)')
+		.pipe(PLUMBER({errorHandler: NOTIFY.onError('Error: <%= error.message %>')}))
 		.pipe(SASS({outputStyle: 'compressed'}))
 		.pipe(POSTCSS(POSTCSS_PLUGINS))
 		.pipe(CONCAT('style.css'))
-		.pipe(GULP.dest('./' + SETTINGS.dist.css));
+		.pipe(GULP.dest('./' + SETTINGS.dist.css))
 })
 
 GULP.task('build-js', function() {
@@ -69,39 +69,40 @@ GULP.task('build-js', function() {
 
 GULP.task('img', function() {
 	return GULP.src(SETTINGS.src.img + '**/*.+(png|jpg|gif|svg)')
-		.pipe(PLUMBER({errorHandler: NOTIFY.onError("Error: <%= error.message %>")}))
+		.pipe(PLUMBER({errorHandler: NOTIFY.onError('Error: <%= error.message %>')}))
 		.pipe(WAIT(500))
 		.pipe(CACHE(IMAGEMIN()))
-		.pipe(GULP.dest('./' + SETTINGS.dist.img));
+		.pipe(GULP.dest('./' + SETTINGS.dist.img))
 
 })
 
 GULP.task('fonts', function() {
 	return GULP.src(SETTINGS.src.fonts + '**/*.*')
-		.pipe(PLUMBER({errorHandler: NOTIFY.onError("Error: <%= error.message %>")}))
-		.pipe(GULP.dest('./' + SETTINGS.dist.fonts));
+		.pipe(PLUMBER({errorHandler: NOTIFY.onError('Error: <%= error.message %>')}))
+		.pipe(GULP.dest('./' + SETTINGS.dist.fonts))
 
 })
 
 GULP.task('html', function() {
 	return GULP.src(SETTINGS.src.root + '**/*.html')
-		.pipe(PLUMBER({errorHandler: NOTIFY.onError("Error: <%= error.message %>")}))
-		.pipe(GULP.dest('./' + SETTINGS.dist.root));
+		.pipe(PLUMBER({errorHandler: NOTIFY.onError('Error: <%= error.message %>')}))
+		.pipe(GULP.dest('./' + SETTINGS.dist.root))
 
 })
 
 GULP.task('clean', function() {
-	return CLEAN.sync('./' + SETTINGS.dist.root);
+	return CLEAN.sync('./' + SETTINGS.dist.root)
 })
 
-GULP.task('dist', ['clean', 'sass:dist', 'build-js', 'img', 'html', 'fonts'])
 
-GULP.task('watch', function() {
-	GULP.watch('./' + SETTINGS.src.scss + "**/*.+(scss|sass)", ['sass:dev']);
-	GULP.watch('./' + SETTINGS.src.root + "**/*.html", ['html']).on('change', BROWSER_SYNC.reload);
-	GULP.watch('./' + SETTINGS.src.js + "**/*.*", ['build-js']).on('change', BROWSER_SYNC.reload);
-	GULP.watch('./' + SETTINGS.src.fonts + "**/*.*", ['fonts']).on('change', BROWSER_SYNC.reload);
-	GULP.watch('./' + SETTINGS.src.img + "**/*.+(png|jpg|gif|svg)", ['img']);
-})
+GULP.task('dist', GULP.series('clean', 'sass:dist', 'build-js', 'img', 'html', 'fonts'))
 
-GULP.task('dev', ['dist', 'watch', 'server'])
+GULP.task('watch', GULP.parallel(
+    css  = () => { GULP.watch('./' + SETTINGS.src.scss + '**/*.+(scss|sass)', GULP.series('sass:dev')) },
+    html = () => { GULP.watch('./' + SETTINGS.src.root + '**/*.html', GULP.series('html')).on('change', BROWSER_SYNC.reload) },
+    js   = () => { GULP.watch('./' + SETTINGS.src.js + '**/*.*', GULP.series('build-js')).on('change', BROWSER_SYNC.reload) },
+    font = () => { GULP.watch('./' + SETTINGS.src.fonts + '**/*.*', GULP.series('fonts')).on('change', BROWSER_SYNC.reload) },
+    img  = () => { GULP.watch('./' + SETTINGS.src.img + '**/*.+(png|jpg|gif|svg)', GULP.series('img')) }
+    ))
+
+GULP.task('dev', GULP.series('dist', 'watch', 'server'))
