242335ed9135bf035e8fc219432283d1d825158f
hyyper <hypercomps@gmail.com>
2022-03-31 23:18:30 -0700

    Refactored

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 const SETTINGS     = require('./gulp-settings.js')
 const GULP         = require('gulp')
-const SASS         = require('gulp-sass')
+const SASS         = require('gulp-sass')(require('sass'))
 const CONCAT       = require('gulp-concat')
-const UGLIFYCSS    = require('gulp-uglifycss')
 const BABEL        = require('gulp-babel')
 const NOTIFY       = require('gulp-notify')
 const PLUMBER      = require('gulp-plumber')
@@ -15,26 +14,12 @@ const IMAGEMIN     = require('gulp-imagemin')
 const WAIT         = require('gulp-wait')
 const CACHE        = require('gulp-cache')
 
-function handleError(error) {
-	console.log(error.toString())
-	this.emit('end')
-}
-
 const POSTCSS_PLUGINS = [
-	AUTOPREFIXER({ browsers: ['last 4 version', 'ie >= 10', 'Android 4'] })
+	AUTOPREFIXER()
 ]
 
-//local server
-GULP.task('server', function() {
-	BROWSER_SYNC.init({
-		server: {
-			baseDir: './' + SETTINGS.dist.root,
-			port: 3010,
-			directory: true,
-			reloadOnRestart: true
-		}
-	})
-})
+// Individual tasks
+// ----------------------------------------------------
 
 GULP.task('sass:dev', function() {
 	return GULP.src(SETTINGS.src.scss + '**/*.+(scss|sass)')
@@ -59,11 +44,14 @@ GULP.task('sass:dist', function() {
 })
 
 GULP.task('build-js', function() {
-	GULP.src('src/js/*')
+	return GULP.src('src/js/*')
 		.pipe(BABEL({
 			plugins: ['@babel/transform-runtime']
 		}))
-		.on('error', handleError)
+		.on('error', function handleError(error) {
+            console.log(error.toString())
+            this.emit('end')
+        })
 		.pipe(GULP.dest('./' + SETTINGS.dist.js))
 })
 
@@ -73,36 +61,50 @@ GULP.task('img', function() {
 		.pipe(WAIT(500))
 		.pipe(CACHE(IMAGEMIN()))
 		.pipe(GULP.dest('./' + SETTINGS.dist.img))
-
 })
 
 GULP.task('fonts', function() {
 	return GULP.src(SETTINGS.src.fonts + '**/*.*')
 		.pipe(PLUMBER({errorHandler: NOTIFY.onError('Error: <%= error.message %>')}))
 		.pipe(GULP.dest('./' + SETTINGS.dist.fonts))
-
 })
 
 GULP.task('html', function() {
 	return GULP.src(SETTINGS.src.root + '**/*.html')
 		.pipe(PLUMBER({errorHandler: NOTIFY.onError('Error: <%= error.message %>')}))
 		.pipe(GULP.dest('./' + SETTINGS.dist.root))
-
 })
 
-GULP.task('clean', function() {
+GULP.task('clean', async function() {
 	return CLEAN.sync('./' + SETTINGS.dist.root)
 })
 
+// ----------------------------------------------------
+
+// Watch
+GULP.task('watch', GULP.parallel(
+    watchCss  = () => { GULP.watch('./' + SETTINGS.src.scss + '**/*.+(scss|sass)', GULP.series('sass:dev')) },
+    watchHtml = () => { GULP.watch('./' + SETTINGS.src.root + '**/*.html', GULP.series('html')).on('change', BROWSER_SYNC.reload) },
+    watchJs   = () => { GULP.watch('./' + SETTINGS.src.js + '**/*.*', GULP.series('build-js')).on('change', BROWSER_SYNC.reload) },
+    watchFont = () => { GULP.watch('./' + SETTINGS.src.fonts + '**/*.*', GULP.series('fonts')).on('change', BROWSER_SYNC.reload) },
+    watchImg  = () => { GULP.watch('./' + SETTINGS.src.img + '**/*.+(png|jpg|gif|svg)', GULP.series('img')) }
+))
+
+// Local server for dist root
+GULP.task('server', async function() {
+	BROWSER_SYNC.init({
+		server: {
+			baseDir: './' + SETTINGS.dist.root,
+			port: 3010,
+			directory: true,
+			reloadOnRestart: true
+		}
+	})
+})
+
+// Main series tasks
+// ----------------------------------------------------
 
 GULP.task('dist', GULP.series('clean', 'sass:dist', 'build-js', 'img', 'html', 'fonts'))
 
-GULP.task('watch', GULP.parallel(
-    css  = () => { GULP.watch('./' + SETTINGS.src.scss + '**/*.+(scss|sass)', GULP.series('sass:dev')) },
-    html = () => { GULP.watch('./' + SETTINGS.src.root + '**/*.html', GULP.series('html')).on('change', BROWSER_SYNC.reload) },
-    js   = () => { GULP.watch('./' + SETTINGS.src.js + '**/*.*', GULP.series('build-js')).on('change', BROWSER_SYNC.reload) },
-    font = () => { GULP.watch('./' + SETTINGS.src.fonts + '**/*.*', GULP.series('fonts')).on('change', BROWSER_SYNC.reload) },
-    img  = () => { GULP.watch('./' + SETTINGS.src.img + '**/*.+(png|jpg|gif|svg)', GULP.series('img')) }
-    ))
-
-GULP.task('dev', GULP.series('dist', 'watch', 'server'))
+GULP.task('dev', GULP.series('dist', 'server', 'watch'))
